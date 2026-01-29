ecd690ffe3569593df3ca8f8f0ce93dda0e7dd7f
Oliver Antoniu <oliver+github@abolish.golf>
2026-01-29 09:23:25 -0700

    rev [02 - reactive patterns]: ex01 - pubsub: Step 6: handle live search via pubsub events

--- 02 -reactive patterns/examples/01 - expense tracker, pub-sub pattern/instructor/js/main.js
+++ 02 -reactive patterns/examples/01 - expense tracker, pub-sub pattern/instructor/js/main.js
@@ -5,9 +5,25 @@ import theExpenses from './expenses-data.js'
 import expenses from './expenses.js'
 
 
-// wire expenseContainer's expenses attribute to the expenses publisher
+// Step 4: wire expenseContainer's expenses attribute to the expenses publisher
 const expenseContainer = document.querySelector('expense-container');
 
 expenses.subscribe("update", (expenses) => {
     expenseContainer.setAttribute('expenses', JSON.stringify(expenses));
+});
+
+// Step 5: pass the data to the pubsub handler
+expenses.clear();
+expenses.addExpense(...theExpenses);
+
+
+// Step 6: handle live search via expenses publisher
+document.getElementById("searchbox").addEventListener("input", (e) => {
+  const input = e.target.value;
+  if (input.length > 0) {
+    expenses.filterExpense(input);
+  } else {
+    expenses.clear();
+    expenses.addExpense(...theExpenses);
+  }
 });
